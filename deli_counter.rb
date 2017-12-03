c61487e974b68d4f952b807bedde920d0be40f80
andreancardona@gmail.com <andreancardona@gmail.com>
2017-12-03 20:05:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,11 @@
 def line(array)
   if array[0] == nil
    puts "The line is currently empty."
+ else 
+  current_line = "The line is currently:"
+  deli.each.with_index(1) do |person, i|
+    current_line << " #{i}. #{person}"
+  end
+  puts current_line
  end
 end
