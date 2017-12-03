4ff676a02c8ef4eefe09e160afcd503eb7cf7c5f
andreancardona@gmail.com <andreancardona@gmail.com>
2017-12-03 20:24:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,8 @@ def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
+else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
